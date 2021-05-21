f7375d4a25680f75189068b097a29778b384b206
Claudio Freitas <claudiofreitas3@gmail.com>
2021-05-21 17:23:41 +0900

    Add prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return (
-    <div>a</div>
-  );
-}
+  return <div>a</div>;
+};
 
 export default App;
--- src/main.js
+++ src/main.js
@@ -4,7 +4,4 @@ import App from './App';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(
-  <App />,
-  root,
-);
+ReactDOM.render(<App />, root);
